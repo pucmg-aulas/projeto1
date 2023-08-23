510f5b622c6f972c7f02cc54d3b24de31aebe1a0
LuisBrescia <luisfelipebrescia@gmail.com>
2023-08-22 22:09:04 -0300

    Esboco do codigo

---
+++ codigo/classes.java
@@ -0,0 +1,58 @@
+// Vários equipamentos
+
+// 1 Classe para todos os equipamento
+
+class Equipamento {
+    // Código Único
+    // Descrição
+}
+
+// Entiadde ~ Equipamento
+
+class Aluguel {
+    // Entidade Cliente
+    // Entidade Equipamento
+    // Valor diário
+    // Data início
+    // Data término
+
+    /* 
+    function valorTotal(){
+        out(valorDiario * dias)
+    }
+    */
+}
+
+/* 
+ * O valor total do aluguel é calculado multiplicando o
+ *  número de dias de aluguel pelo valor diário do equipamento.
+*/
+
+
+// O sistema deve ser capaz de:
+
+// 1 - Registrar novos aluguéis, incluindo todas as informações necessárias.
+// new novoAluguel = Aluguel(Cliente1, Equipamento1, 50.00, 12/06, 23/09)
+
+// 2 - Para ver o valor total
+// novoAluguel.valorTotal();
+
+// 3 - Manter um registro dos aluguéis passados, incluindo informações sobre o cliente, o equipamento e as datas de início e término.
+// vetorAlugueis.push(aluguelFinalizado)
+
+// vetorAlugueis.push(novoAluguel);
+
+// 4 - Permitir a consulta rápida dos aluguéis atuais e passados de um cliente específico.
+// aluguelAtivo[], aluguelFinalizado[] - 
+/* 
+function consultaCliente(nomeCliente){
+    // Loop pelos vetores aluguelAtivo e aluguelFinalizado, caso o nome do cliente bata, imprima na tela
+}
+*/
+
+// 5 - Fornecer um relatório mensal que liste todos os aluguéis do mês, incluindo o faturamento total.
+/* 
+function relatorioMensal(nomeCliente){
+    // Imprimir valores dos alugueis fechados daquele mes 
+}
+*/
