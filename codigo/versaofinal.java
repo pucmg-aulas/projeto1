40c9dbf13ead420eb30cd6912b783e7d258949d0
Luis Brescia <luisfelipebrescia@gmail.com>
2023-09-12 20:37:05 -0300

    Adiiconada função calcularFaturamentoMensal, formatação melhorada, Documentação adicionada

--- codigo/versaofinal.java
+++ codigo/versaofinal.java
@@ -1,3 +1,44 @@
+/*  ? Documentação:
+
+    consultaCliente(Cliente cliente, List<Aluguel> alugueis)
+    Retorna uma lista de aluguéis de um cliente específico.
+
+    relatorioMensal(int mes, int ano, List<Aluguel> alugueis)
+    Retorna o faturamento total de um mês e ano específicos.
+
+    criarAluguel(Scanner scanner, List<Aluguel> alugueis)
+    Cria um novo aluguel e adiciona na lista de aluguéis.
+
+    Equipamento
+    Classe que representa um equipamento.
+    Possui os atributos código, descrição e valor diário.
+
+    Cliente
+    Classe que representa um cliente.
+    Possui apenas o atributo nome.
+
+    Aluguel
+    Classe que representa um aluguel.
+    Possui métodos estáticos para consultar aluguéis de um cliente específico e calcular o faturamento mensal.
+
+    main(String[] args)
+    Função principal do programa.
+
+    O programa possui um menu com as seguintes opções:
+    1 - Consultar aluguéis de cliente específico
+    2 - Gerar relatório mensal
+    3 - Criar aluguel
+    4 - Sair
+
+    A opção 1 pede o nome do cliente e retorna os aluguéis do cliente.
+    A opção 2 pede o mês e ano e retorna o faturamento total do mês e ano.
+    A opção 3 pede o nome do cliente, o equipamento, a data de início e a data de término do aluguel.
+    A opção 4 sai do programa.
+
+    O programa possui uma lista de aluguéis que é passada como parâmetro para os métodos.
+    A lista de aluguéis é inicializada com dois aluguéis de exemplo.
+ */
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -13,41 +54,42 @@ public class versaofinal {
 
         List<Aluguel> alugueis = new ArrayList<>(); 
 
-        int escolha;
-        do {
-            System.out.println("Escolha uma opção:");
-            System.out.println("1. Consultar aluguéis do cliente");
-            System.out.println("2. Gerar relatório mensal");
-            System.out.println("3. Criar aluguel");
-            System.out.println("4. Sair");
+        int escolha = 0;
+
+        while (escolha != 4) {
+            System.out.println("\nEscolha uma opção:\n");
+            System.out.println("1 - Consultar aluguéis de cliente específico");
+            System.out.println("2 - Gerar relatório mensal");
+            System.out.println("3 - Criar aluguel");
+            System.out.println("4 - Sair\n");
 
             escolha = scanner.nextInt();
 
             switch (escolha) {
                 case 1:
                     scanner.nextLine();
-                    System.out.println("Digite o nome do cliente:");
+                    System.out.println("\nDigite o nome do cliente:");
                     String nomeCliente = scanner.nextLine();
                     Cliente cliente = new Cliente(nomeCliente);
                     consultaCliente(cliente, alugueis);
                     break;
                 case 2:
-                    System.out.println("Digite o mês (1-12):");
+                    System.out.println("\nDigite o mês (1-12):");
                     int mes = scanner.nextInt();
                     System.out.println("Digite o ano:");
                     int ano = scanner.nextInt();
-                    relatorioMensal(mes, ano, alugueis);
+                     relatorioMensal(mes, ano, alugueis);
                     break;
                 case 3:
-                    criarAluguel(scanner, alugueis);
+                     criarAluguel(scanner, alugueis);
                     break;
                 case 4:
-                    System.out.println("Saindo do programa.");
+                     System.out.println("Saindo do programa.");
                     break;
                 default:
                     System.out.println("Opção inválida. Tente novamente.");
             }
-        } while (escolha != 4);
+        } 
 
         scanner.close();
     }
@@ -55,12 +97,16 @@ public class versaofinal {
     public static void consultaCliente(Cliente cliente, List<Aluguel> alugueis) {
         List<Aluguel> alugueisCliente = Aluguel.consultarAlugueisCliente(cliente, alugueis);
 
-        System.out.println("Aluguéis do cliente " + cliente.getNome() + ":");
+        System.out.println("\nAluguéis do cliente " + cliente.getNome() + ":");
         for (Aluguel aluguel : alugueisCliente) {
             System.out.println("Equipamento: " + aluguel.getEquipamento().getDescricao()
                     + " | Data Início: " + aluguel.getDataInicio()
                     + " | Data Término: " + aluguel.getDataTermino()
                     + " | Valor Total: " + aluguel.getValorTotal());
+        }
+
+        if (alugueisCliente.size() == 0) {
+            System.out.println("Nenhum aluguel encontrado.");
         }
     }
 
@@ -90,9 +136,9 @@ public class versaofinal {
             return;
         }
 
-        System.out.println("Digite a data de início (yyyy-MM-dd):");
+        System.out.println("Digite a data de início (YYYY-MM-DD):");
         String dataInicio = scanner.nextLine();
-        System.out.println("Digite a data de término (yyyy-MM-dd):");
+        System.out.println("Digite a data de término (YYYY-MM-DD):");
         String dataTermino = scanner.nextLine();
 
         Aluguel aluguel = new Aluguel(cliente, equipamento, dataInicio, dataTermino);
@@ -180,8 +226,39 @@ public class versaofinal {
         public static double calcularFaturamentoMensal(int mes, int ano, List<Aluguel> alugueis) {
             double faturamentoTotal = 0.0;
             for (Aluguel aluguel : alugueis) {
-                // Verificação do mês e ano especificados do aluguel
-                // e somar o valor ao faturamentoTotal
+
+                String[] dataInicio = aluguel.getDataInicio().split("-");
+                String[] dataTermino = aluguel.getDataTermino().split("-");
+                int mesInicio = Integer.parseInt(dataInicio[1]);
+                int anoInicio = Integer.parseInt(dataInicio[0]);
+                int mesTermino = Integer.parseInt(dataTermino[1]);
+                int anoTermino = Integer.parseInt(dataTermino[0]);
+
+                if (anoInicio == ano && anoTermino == ano) {
+                    if (mesInicio == mes && mesTermino == mes) {
+                        faturamentoTotal += aluguel.getValorTotal();
+                    } else if (mesInicio == mes && mesTermino > mes) {
+                        faturamentoTotal += aluguel.getValorTotal();
+                    } else if (mesInicio < mes && mesTermino == mes) {
+                        faturamentoTotal += aluguel.getValorTotal();
+                    } else if (mesInicio < mes && mesTermino > mes) {
+                        faturamentoTotal += aluguel.getValorTotal();
+                    }
+                } else if (anoInicio == ano && anoTermino > ano) {
+                    if (mesInicio == mes) {
+                        faturamentoTotal += aluguel.getValorTotal();
+                    } else if (mesInicio < mes) {
+                        faturamentoTotal += aluguel.getValorTotal();
+                    }
+                } else if (anoInicio < ano && anoTermino == ano) {
+                    if (mesTermino == mes) {
+                        faturamentoTotal += aluguel.getValorTotal();
+                    } else if (mesTermino > mes) {
+                        faturamentoTotal += aluguel.getValorTotal();
+                    }
+                } else if (anoInicio < ano && anoTermino > ano) {
+                    faturamentoTotal += aluguel.getValorTotal();
+                }
             }
             return faturamentoTotal;
         }
